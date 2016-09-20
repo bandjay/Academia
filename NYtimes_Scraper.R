08796818167c99e35a392fcc1deac1332694d6c4
bandjay <bandlmd2@illinois.edu>
2016-09-19 21:43:35 -0500

    new version

--- NYtimes_Scraper.R
+++ NYtimes_Scraper.R
@@ -12,7 +12,7 @@ for (i in 1:2)
     
     for (k in 1:10)
     {
-     x<- sample(1:52, 10, replace=F)
+     x<- sample(1:52, 20, replace=F)
      Date_mat[j,k]=format(Date+(x[k]*7), format="%Y/%m/%d")
     }
     Date=Date+1
@@ -20,5 +20,42 @@ for (i in 1:2)
   Date=Date+(365*i)
 }
 
-articles=as_search(q="immigration", begin_date =gsub("-","",Date) , end_date = "20160131",page = 0,fl="lead_paragraph,abstract,headline,source",facet_feild="section_name,document_type,type_of_material",hl=TRUE)
+articles=as_search(q="immigration", begin_date ="20160101" , end_date = "20160131",page = 1,
+                   fl="web_url,snippet,lead_paragraph,abstract,headline,source,document_type,word_count",
+                   facet_field="section_name,type_of_material",hl=TRUE)
+begin_date ="20160101"
+date=c(rep(NA,10))
+web_url=c(rep(NA,10))
+snippet=rep(NA,10)
+lead_paragraph=rep(NA,10)
+abstract=c(rep(NA,10))
+headline=rep(NA,10)
+document_type=rep(NA,10)
+word_count=rep(NA,10)
+section_name=rep(NA,10)
+type_of_material=rep(NA,10)
+for (i in 1:10)
+{
+  date[i]=begin_date
+  web_url[i]=ifelse(is.null(articles$data$docs[[i]]$web_url),"NA",articles$data$docs[[i]]$web_url)
+  snippet[i]=ifelse(is.null(articles$data$docs[[i]]$snippet),"NA",articles$data$docs[[i]]$snippet)
+  lead_paragraph[i]=ifelse(is.null(articles$data$docs[[i]]$lead_paragraph),"NA",articles$data$docs[[i]]$lead_paragraph)
+  abstract[i]=ifelse(is.null(articles$data$docs[[i]]$abstract),"NA",articles$data$docs[[i]]$abstract)
+  headline[i]=ifelse(is.null(articles$data$docs[[i]]$headline$main),"NA",articles$data$docs[[i]]$headline$main)
+  document_type[i]=ifelse(is.null(articles$data$docs[[i]]$document_type),"NA",articles$data$docs[[i]]$document_type)
+  word_count[i]=ifelse(is.null(articles$data$docs[[i]]$word_count),"NA",articles$data$docs[[i]]$word_count)
+  #section_name[i]=ifelse(is.null(articles$data$facets$section_name$terms[[i]]$term),"NA",articles$data$facets$section_name$terms[[i]]$term)
+  #type_of_material[i]=ifelse(is.null(articles$data$facets$type_of_material$terms[[i]]$term),"NA",articles$data$facets$type_of_material$terms[[i]]$term)
+}
+
+
+
+
+
+
+
+
+
+
+
 
